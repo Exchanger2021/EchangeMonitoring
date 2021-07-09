aea148dec315061bc723acc30a29e36cda3efb0e
Sanjello <sanjello3@gmail.com>
2021-07-09 13:12:42 +0300

    added monitoring block

---
+++ wp-content/themes/exchangemonitoring/monitoring.php
@@ -0,0 +1,25 @@
+<div class="monitoring">
+    <div class="monitoring_wrapper">
+        <div class="monitoring_info__wrapper">
+            <h2>Моніторинг обмінників</h2>
+            <p>Шукаєте хороший обмінник криптовалют онлайн? Наш сервіс уже підібрав для Вас список надійних обмінників
+                за навигіднішим курсом.
+                Нижче в таблиці представлено список найпопулярніших напрямків. Для обміну будь-якої іншої криптовалюти
+                пропонуємо скористатись нашим калькулятором.</p>
+        </div>
+        <div class="monitoring_table__wrapper">
+            <table border="0" max-width="100%" width='100%' cellpadding='0' cellspacing='0' style='border-collapse:collapse' >
+                <tr class="monitoring_table__head" >
+                    <th>Обмінник </th>
+                    <th>Віддаєте</th>
+                    <th>Отримаєте</th>
+                    <th>Резерв</th>
+                    <th>Відгуки</th>
+                </tr>
+                <tr>
+                    <td>Обмінник </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
