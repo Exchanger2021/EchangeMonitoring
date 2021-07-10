7755a1814366657b17f2cf523d69f112b8b1ff5a
Sanjello <sanjello3@gmail.com>
2021-07-10 17:09:40 +0300

    Translate content, visual changes to calculator and monitoring,turn on icons

--- wp-content/themes/exchangemonitoring/calculator.php
+++ wp-content/themes/exchangemonitoring/calculator.php
@@ -1,115 +1,125 @@
 <div class="calculator">
     <div class="calculator_item_header">
-        Калькулятор<br>криптовалюти
+        Калькулятор<br>криптовалют
+    </div>
+    <form action="#" class="calculator_sum">
         <div>
-            <form action="#" class="calculator_sum">
-                <div>
-                    <p class="calculator_p">Сума:</p>
-                    <p><input type="number" name="num" min="0"></p>
-                </div>
-            </form>
-            <div class="calculator_info_wrapper">
-                <div class="calculator_search_1">
+            <p class="calculator_p">Сума:</p>
+            <p><input type="number" name="num" min="0"></p>
+        </div>
+    </form>
+    <div class="calculator_info_wrapper">
+        <div class="calculator_search">
+            Bitcoin(BTC)
+        </div>
+        <div class="calculator_arrow">
+            <img src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/right-arrow.png">
+        </div>
+        <div class="calculator_search">
+            Ethereum(ETH)
+        </div>
+    </div>
+    <div class="tablewrapper">
+        <table border="0" max-width="100%" width='100%' cellpadding='0' cellspacing='0'
+            style='border-collapse:collapse'>
+            <tr>
+                <td> <img src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/bitcoin.png">
                     Bitcoin(BTC)
-                </div>
-                <div class="calculator_arrow">
-                    <img src="assets\images\calculator\right-arrow.png">
-                </div>
-                <div class="calculator_search_2">
-                    Ethereum(ETH)
-                </div>
-            </div>
-            <div class="tablewrapper">
-                <div class="tablewrapper_1">
-                    <table border="0" max-width="100%" width='100%' cellpadding='0' cellspacing='0'
-                        style='border-collapse:collapse'>
-                        <tr>
-                            <td> <img src="assets/images/calculator/bitcoin.png"> Bitcoin(BTC)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/ethereum.png">Ethereum(ETH)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/tether.png">Tether(USDT)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/binance.png">Binance Coin(BNB)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/coin.png">XRP(XRP)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/dogecoin.png">Dogecoin(DOGE)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/polkadot.png">Polkadot(DOT)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/qtum.png">Qtum(QTUM)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                    </table>
 
-                    <table border="0" max-width="100%" width='100%' cellpadding='0' cellspacing='0'
-                        style='border-collapse:collapse'>
-                        <tr>
-                            <td> <img src="assets/images/calculator/bitcoin.png"> Bitcoin(BTC)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/ethereum.png">Ethereum(ETH)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/tether.png">Tether(USDT)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/binance.png">Binance Coin(BNB)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/coin.png">XRP(XRP)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/dogecoin.png">Dogecoin(DOGE)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/polkadot.png">Polkadot(DOT)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="assets/images/calculator/qtum.png">Qtum(QTUM)
-                                <hr size="2" color="C4C4C4" />
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-		</div>
-	</div>
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/ethereum.png">Ethereum(ETH)
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/tether.png">Tether(USDT)
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/binance.png">Binance
+                    Coin(BNB)
+
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/coin.png">XRP(XRP)
+
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/dogecoin.png">Dogecoin(DOGE)
+
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/polkadot.png">Polkadot(DOT)
+
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/qtum.png">Qtum(QTUM)
+
+                </td>
+            </tr>
+        </table>
+
+        <table border="0" max-width="100%" width='100%' cellpadding='0' cellspacing='0'
+            style='border-collapse:collapse'>
+            <tr>
+                <td> <img src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/bitcoin.png">
+                    Bitcoin(BTC)
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/ethereum.png">Ethereum(ETH)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/tether.png">Tether(USDT)
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/binance.png">Binance
+                    Coin(BNB)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/coin.png">XRP(XRP)
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/dogecoin.png">Dogecoin(DOGE)
+
+                </td>
+            </tr>
+            <tr>
+                <td><img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/polkadot.png">Polkadot(DOT)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img
+                        src="/wordpress/wp-content/themes/exchangemonitoring/assets/images/calculator/qtum.png">Qtum(QTUM)
+                </td>
+            </tr>
+        </table>
+    </div>
 </div>
